87ae409a46d8a06ec3ebad76efb86f47cc22301e
kuboy275 <caubebandu96@gmail.com>
2024-03-16 16:14:12 +0700

    Update code

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,59 @@
-"use strict";function addReceiverInput(){$(".js-add-receiver-input").on("click",function(){$(".add-receiver-wrapper").append('<div class="form-row multiple-input"><div class="col-8"><div class="form-group"><input class="form-control" type="text" name="receiver-address-field[]" placeholder="Enter receiver address"></div></div><div class="col-4"><div class="form-group"><input class="form-control" type="text" name="receiver-amount[]" placeholder="How much"> <button class="btn btn-icon js-remove-receiver-input" type="button"><i class="far fa-times"></i></button></div></div></div>')}),$(document).on("click",".js-remove-receiver-input",function(){$(this).parents(".form-row.multiple-input").remove()})}function openMenuMobile(){$(".btn-open-menu").on("click",function(){$("html").toggleClass("overflow-hidden"),$(".navbar").toggleClass("active"),$(this).toggleClass("opened")})}function uploadFileLogo(){$(".js-upload-logo").on("change",function(e){var o=$(".label-upload-logo img"),n=(n=$(this).val()).substring(n.lastIndexOf("\\")+1),a=new FileReader;a.onload=function(e){o.attr("src",e.target.result),$(".label-upload-logo .desc p").hide(),$(".label-upload-logo .desc p.name-img").text(n).show()},a.readAsDataURL(this.files[0])})}function autoShowDropdownNavbar(){$(".navbar .navbar-item.dropdown").each(function(e,o){$(o).find(".collapse .navbar-link").hasClass("active")&&$(o).find(".collapse .navbar-link").parents(".collapse").collapse("show")})}$(document).ready(function(){addReceiverInput(),openMenuMobile(),uploadFileLogo(),autoShowDropdownNavbar(),$(window).on("load",function(){$("#modalConnectWallet").modal("hide")})});
+function addReceiverInput() {
+    $(".js-add-receiver-input").on('click', function(){
+        $('.add-receiver-wrapper').append('<div class="form-row multiple-input"><div class="col-8"><div class="form-group"><input class="form-control" type="text" name="receiver-address-field[]" placeholder="Enter receiver address"></div></div><div class="col-4"><div class="form-group"><input class="form-control" type="text" name="receiver-amount[]" placeholder="How much"> <button class="btn btn-icon js-remove-receiver-input" type="button"><i class="far fa-times"></i></button></div></div></div>');
+    });
+
+    $(document).on('click', '.js-remove-receiver-input', function(){
+        $(this).parents('.form-row.multiple-input').remove();
+    })
+}
+
+function openMenuMobile() {
+    $('.btn-open-menu').on('click', function(){
+        $('html').toggleClass('overflow-hidden');
+        $('.navbar').toggleClass('active');
+        $(this).toggleClass('opened');
+    })
+}
+
+function uploadFileLogo() {
+    $('.js-upload-logo').on('change', function(e) {
+        var currentEle = $('.label-upload-logo img');
+        var fileName = $(this).val();
+        fileName= fileName.substring(fileName.lastIndexOf('\\') + 1);
+        var reader = new FileReader();
+        reader.onload = function(event) {
+            currentEle.attr('src', event.target.result);
+            $('.label-upload-logo .desc p').hide();
+            $('.label-upload-logo .desc p.name-img').text(fileName).show();
+        }
+        reader.readAsDataURL(this.files[0])
+    })
+}
+
+function autoShowDropdownNavbar() {
+    $('.navbar .navbar-item.dropdown').each(function(id, el) {
+        if( $(el).find('.collapse .navbar-link').hasClass('active') ) {
+            $(el).find('.collapse .navbar-link').parents('.collapse').collapse('show')
+        }
+    })
+}
+
+
+
+$(document).ready(function () {
+    // Add Receiver Address Input
+    addReceiverInput();
+
+    // OpenMenuMobile
+    openMenuMobile();
+
+    // Upload Logo
+    uploadFileLogo();
+
+    autoShowDropdownNavbar();
+    
+    $(window).on('load', function() {
+        $('#modalConnectWallet').modal('hide');
+    });
+});
